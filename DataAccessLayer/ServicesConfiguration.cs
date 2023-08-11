d9771f412efc1b62a2163bbf282b5f3c59068570
Geraldsonp <Geraldson.a.P@gmail.com>
2023-08-10 22:59:58 -0400

    Adds ServicesConfiguration.cs in DAL

---
+++ DataAccessLayer/ServicesConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccessLayer;
+
+public static class ServicesConfiguration
+{
+    public static IServiceCollection RegisterDalServices(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("DbConnection");
+        services.AddDbContext<AccountMngDbContext>(options => options.UseSqlServer(connectionString));
+        
+        
+        return services;
+    }
+}
