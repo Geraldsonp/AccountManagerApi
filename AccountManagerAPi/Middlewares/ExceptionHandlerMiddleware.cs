267ed1169d8cea14cf12527acb57e54686e49d9f
Geraldsonp <Geraldson.a.P@gmail.com>
2023-08-13 20:04:10 -0400

    adds Loggin

--- AccountManagerAPi/Middlewares/ExceptionHandlerMiddleware.cs
+++ AccountManagerAPi/Middlewares/ExceptionHandlerMiddleware.cs
@@ -10,29 +10,42 @@ namespace AccountManagerAPi.Middlewares
     public class ExceptionHandlerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
 
-        public ExceptionHandlerMiddleware(RequestDelegate next)
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
+
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
+                _logger.LogInformation("Handling request: " + context.Request.Path);
+                _logger.LogInformation("Request method: " + context.Request.Method);
                 await _next(context);
             }
             catch (DomainException ex)
             {
+                _logger.LogError(ex, ex.Message);
                 await HandleNotFoundExceptionAsync(context, ex);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, ex.Message);
                 await HandleExceptionAsync(context, ex);
             }
         }
+
+
+
         private static Task HandleNotFoundExceptionAsync(HttpContext context, DomainException ex)
         {
+
+
             var result = JsonSerializer.Serialize(new { error = ex.Message });
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = ex.StatusCode;
